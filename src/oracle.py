585f88300959aeabefe23c267e017066b7e2e2e2
mxttbrunet <brunetm2@winthrop.edu>
2024-06-20 11:50:13 -0400

    added ConverterToESOP class with to_esop() function using ANF.... needs optimization

--- src/oracle.py
+++ src/oracle.py
@@ -17,9 +17,9 @@ symbolsAvail = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p]
 
 
 class BooleanExpressionGenerator:          
-    def __init__(self, numVars,listOfMinterms):         ##initial class parameters 
+    def __init__(self, numVars,feasibleStates):         ##initial class parameters 
         self.numVars = numVars                          ##number of variables 
-        self.listOfMinterms = listOfMinterms         
+        self.feasibleStates = feasibleStates        
 
         allOnesState = []
         theseSymbols = []                               ##dummy lists
@@ -28,17 +28,23 @@ class BooleanExpressionGenerator:
             theseSymbols.append(symbolsAvail[i])        ## also populates the "feasible" all ones state to be removed possibly
             allOnesState.append(1)
 
-        if((allOnesState in self.listOfMinterms)):      ##removes all one's state if included
+        if((allOnesState in self.feasibleStates)):      ##removes all one's state if included
             self.listOfMinterms.remove(allOnesState)    ##removing all one's will be useful with esop
                                                     
-
         self.symbols = theseSymbols                     ##symbols for this expression
 
     def generateExpression(self, form):                 ##pos or sop expression
         form.upper()
         if (form == "SOP"):
-            return sp.logic.boolalg.SOPform(self.symbols, self.listOfMinterms, None)   
+            return sp.logic.boolalg.SOPform(self.symbols, self.feasibleStates, None)   
         elif (form == "POS"):
-            return sp.logic.boolalg.POSform(self.symbols, self.listOfMinterms, None)
+            return sp.logic.boolalg.POSform(self.symbols, self.feasibleStates, None)
         else:
             print("ERROR\n")
+
+class ESOPConverter:
+    def __init__(self,boolExpression, form):  #form variation may be more useful depending on optimization...
+        self.boolExpression = boolExpression
+        self.form = form.upper()
+    def to_esop(self):
+        return sp.logic.boolalg.to_anf(self.boolExpression , True)  #unoptimized...
