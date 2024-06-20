4aa26e9c8ccab1a5821a9ad8d1b564de3aa2e4a1
shilpishah <shilpishah2003@gmail.com>
2024-06-20 12:49:42 -0400

    Add files via upload
    Takes in a networkx graph as input, finds and returns set of ALL feasible solutions (in terms of vertex number)

---
+++ src/networkx_to_feasible_sols.py
@@ -0,0 +1,43 @@
+import networkx as nx
+
+def vertex_cover(graph, verts):
+    covered_edges = set()
+    for v in verts:
+        for neighbor in graph.neighbors(v):
+            covered_edges.add(frozenset((v, neighbor)))
+    return len(covered_edges) == graph.number_of_edges()
+
+def subsets(nodes, i=0, current_set=None, all_sets=None):
+    if current_set is None:
+        current_set = []
+    if all_sets is None:
+        all_sets = []
+    if i == len(nodes):
+        all_sets.append(current_set.copy())
+        return all_sets
+
+    current_set.append(nodes[i]) # include current node
+    subsets(nodes, i + 1, current_set, all_sets)
+
+    current_set.pop() # exclude current node
+    subsets(nodes, i + 1, current_set, all_sets)
+
+    return all_sets
+
+def find_feasible_sols(graph):
+    nodes = list(graph.nodes())
+    subset = subsets(nodes)
+    feasible_sols = []
+    for s in subset:
+        if vertex_cover(graph, s):
+            feasible_sols.append(set(s))
+
+    return feasible_sols
+
+if __name__ == '__main__':
+    G = nx.Graph()
+    G.add_edges_from([(1,2),(2,3),(3,4),(4,5)]) # example graph
+    feasible_cover = find_feasible_sols(G)
+
+    for sol in feasible_cover:
+        print(sol)
