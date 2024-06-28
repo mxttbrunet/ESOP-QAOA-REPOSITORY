6d40e5311d23745a7bcc703783d558709d565bd4
shilpishah <shilpishah2003@gmail.com>
2024-06-28 14:12:16 -0400

    Update sympy_esop_to_qcirc.py
    Take in an example esop expression and convert to quantum circuit

--- src/sympy_esop_to_qcirc.py
+++ src/sympy_esop_to_qcirc.py
@@ -1,44 +1,46 @@
 import sympy as sp
 from qiskit import QuantumCircuit
 from qiskit.visualization import plot_histogram
-# import matplotlib.pyplot as plt --> no longer needed
+import matplotlib.pyplot as plt
 
 class ESOPQuantumCircuit:
-    def __init__(self):
-        self.num_vars = int(input("Enter the number of variables: "))
-        self.vars = sp.symbols(' '.join(f'x{i}' for i in range(self.num_vars)))
-        self.truth_table = self.input_truth_table(self.num_vars)
-        self.ESOP = self.find_ESOP_fxn(self.truth_table, self.vars)
+    def __init__(self, esop_expr):
+        #self.num_vars = int(input("Enter the number of variables: "))
+        #self.vars = sp.symbols(' '.join(f'x{i}' for i in range(self.num_vars)))
+        #self.truth_table = self.input_truth_table(self.num_vars)
+        self.ESOP = esop_expr
+        self.vars = sorted(esop_expr.free_symbols, key=lambda x: str(x))
+        self.num_vars = len(self.vars)
         print("Exclusive Sum of Products (ESOP):", self.ESOP)
 
-        self.qc = self.esop_to_quantum_circuit(self.ESOP, list(self.vars))
+        self.qc = self.esop_to_quantum_circuit(self.ESOP, self.vars)
         print("Quantum Circuit:")
         print(self.qc)
         self.qc.draw(output='mpl')
         # plt.show() --> makes run time much greater, removed for simplicity sake
 
-    def input_truth_table(self, input_num):
-        truth_table_init = []
-        for i in range(2**input_num):
-            vals = list(map(int, input(f"Enter values for each variable separated by spaces for row {i + 1}: ").split()))
-            result = int(input(f"Enter result for row {i + 1}: "))  # can change this in future to automate results based on operation
-            truth_table_init.append((*vals, result))
-        print("Truth table:", truth_table_init)
-        return truth_table_init
+    # def input_truth_table(self, input_num):
+    #     truth_table_init = []
+    #     for i in range(2**input_num):
+    #         vals = list(map(int, input(f"Enter values for each variable separated by spaces for row {i + 1}: ").split()))
+    #         result = int(input(f"Enter result for row {i + 1}: "))  # can change this in future to automate results based on operation
+    #         truth_table_init.append((*vals, result))
+    #     print("Truth table:", truth_table_init)
+    #     return truth_table_init
 
-    def find_ESOP_fxn(self, tt, vars):
-        terms = []
-        for row in tt:
-            vals, result = row[:-1], row[-1]
-            if result == 1:
-                terms_new = []
-                for i, var in enumerate(vars):
-                    if vals[i] == 0:
-                        terms_new.append(~var)
-                    else:
-                        terms_new.append(var)
-                terms.append(sp.And(*terms_new))
-        return sp.Xor(*terms)
+    # def find_ESOP_fxn(self, tt, vars):
+    #     terms = []
+    #     for row in tt:
+    #         vals, result = row[:-1], row[-1]
+    #         if result == 1:
+    #             terms_new = []
+    #             for i, var in enumerate(vars):
+    #                 if vals[i] == 0:
+    #                     terms_new.append(~var)
+    #                 else:
+    #                     terms_new.append(var)
+    #             terms.append(sp.And(*terms_new))
+    #     return sp.Xor(*terms)
 
     def esop_to_quantum_circuit(self, ESOP, vars):
         num_qubits = len(vars) + 1  # Add one extra qubit for the output
@@ -59,8 +61,7 @@ class ESOPQuantumCircuit:
             if len(control_qubits) == 1:
                 qc.mcx(control_qubits[0], num_qubits - 1)  # CNOT for single control --> changed to multi-controlled x gate
             elif len(control_qubits) == 2:
-                qc.mcx(control_qubits[0], control_qubits[1], num_qubits - 1)  # Toffoli for double control --> changed to multi-controlled x gate
-                # for m vertices and n edges, this code's wcs runtime is n(m-1)
+                qc.mcx(control_qubits, num_qubits - 1)  # Toffoli for double control --> changed to multi-controlled x gate
 
             for literal in term.args if term.func == sp.And else [term]:
                 if isinstance(literal, sp.Not):
@@ -69,4 +70,7 @@ class ESOPQuantumCircuit:
         return qc
 
 if __name__ == "__main__":
-    esop_qc = ESOPQuantumCircuit()
+    # example esop -- (x0 & ~x1) ^ (x2 & x3)
+    x0, x1, x2, x3 = sp.symbols('x0 x1 x2 x3')
+    esop_expr = sp.Xor(x0 & ~x1, x2 & x3)
+    esop_qc = ESOPQuantumCircuit(esop_expr)
